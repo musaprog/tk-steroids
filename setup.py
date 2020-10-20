2adcfc394beaee643f4264e8489e56dfb448fac8
Joni <jjtkemppainen1@sheffield.ac.uk>
2020-10-20 21:22:18 +0100

    List dependencies in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+install_requires = [
+        'tkinter',
+        'PIL',
+        'numpy',
+        'matplotlib',
+        ]
+
 setuptools.setup(
     name="tk_steroids-jkemppainen", # Replace with your own username
     version="0.0.1",
@@ -13,6 +20,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jkemppainen/tk_steroids",
     packages=setuptools.find_packages(),
+    install_requires=install_requires
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3) ",
