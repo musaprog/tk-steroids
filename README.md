d2bbeeed5ff140d72d81697e41be5244e41e2ea9
Joni <jjtkemppainen1@sheffield.ac.uk>
2020-10-20 20:48:39 +0100

    Package the project properly

---
+++ README.md
@@ -0,0 +1,5 @@
+Tkinter steroids (tk_steroids)
+
+Contains some useful GUI elements (tkinter widgets) that keep reoccuring
+in the code elsewhere, such as a multiple selection box or matplotlib
+integration using matplotlib's tkinter backend.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tk_steroids-jkemppainen", # Replace with your own username
+    version="0.0.1",
+    author="Joni Kemppainen",
+    author_email="jjtkemppainen1@sheffield.ac.uk",
+    description="A collection of custom tkinter bits",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jkemppainen/tk_steroids",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3) ",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
